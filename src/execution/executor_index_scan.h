d0334b294b2315b9dfe135e4ee9e62dbf7a9eb01
Kyoooma <2856437250@qq.com>
2023-07-10 13:47:47 +0800

    添加index扫描的表锁

--- src/execution/executor_index_scan.h
+++ src/execution/executor_index_scan.h
@@ -80,6 +80,7 @@ public:
             }
         }
         fed_conds_ = conds_;
+        context_->lock_mgr_->lock_shared_on_table(context_->txn_, sm_manager_->fhs_[tab_name_]->GetFd());
     }
 
     std::string getType() override { return "IndexScanExecutor"; };
