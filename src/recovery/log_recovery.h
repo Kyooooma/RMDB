2269ff263d89809fc64beaaaa15644194351192f
Zhongtyu <105589990+Zhongtyu@users.noreply.github.com>
2023-07-12 12:19:03 +0800

    可能能恢复了？

--- src/recovery/log_recovery.h
+++ src/recovery/log_recovery.h
@@ -25,10 +25,11 @@ public:
 
 class RecoveryManager {
 public:
-    RecoveryManager(DiskManager* disk_manager, BufferPoolManager* buffer_pool_manager, SmManager* sm_manager) {
+    RecoveryManager(DiskManager* disk_manager, BufferPoolManager* buffer_pool_manager, SmManager* sm_manager, LogManager* log_manager) {
         disk_manager_ = disk_manager;
         buffer_pool_manager_ = buffer_pool_manager;
         sm_manager_ = sm_manager;
+        log_manager_ = log_manager;
     }
     void analyze();
     void redo();
@@ -40,4 +41,5 @@ private:
     DiskManager* disk_manager_;                                     // 用来读写文件
     BufferPoolManager* buffer_pool_manager_;                        // 对页面进行读写
     SmManager* sm_manager_;                                         // 访问数据库元数据
+    LogManager* log_manager_;
 };
